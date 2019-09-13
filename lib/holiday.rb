17ffac95ff48c1c89f3abf20ce2676f837979033
Matt Plichta <bigcatplichta@gmail.com>
2019-09-13 22:05:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each { |season, holiday| 
+  holiday_hash.each do |season, (holiday, supplies)| 
+    binding.pry
+    holiday.each do |
+  end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
