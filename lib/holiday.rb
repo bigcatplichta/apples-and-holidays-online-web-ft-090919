b8bab91d89f0688317f3d6fd61c69d167f67e21a
Matt Plichta <bigcatplichta@gmail.com>
2019-09-13 22:53:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
       if holiday.to_s.include?("_")
         holiday = holiday.to_s.split("_")
         holiday = holiday.collect {|w| w.capitalize}
+        holiday = holiday.join(" ") + ": "
         binding.pry
       else 
         holiday = holiday.capitalize.to_s + ": "
