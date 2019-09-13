10375ee9090fbf17efae86cbc8ea842eb954bf16
Matt Plichta <bigcatplichta@gmail.com>
2019-09-13 22:30:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,6 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday| 
-    
     holiday.each do |holiday, supplies|
       holiday = holiday.to_s.split("_")
       puts season.to_s.capitalize
