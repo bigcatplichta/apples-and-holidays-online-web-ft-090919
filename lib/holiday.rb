095f1be061c4752c1a4525e559d4c22688bcdb4e
Matt Plichta <bigcatplichta@gmail.com>
2019-09-13 23:11:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   bbq_days = []
   holiday_hash.each do |season, holiday|
-    # binding.pry
+    binding.pry
     bbq_days << holiday.keys if holiday.values.include?("BBQ")
   end
   bbq_days
